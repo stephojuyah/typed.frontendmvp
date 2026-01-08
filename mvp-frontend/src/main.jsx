99f35458048b154841dfa1d5dff294aa3df123d8
stephojuyah <stephojuyah2@gmail.com>
2026-01-08 14:07:31 +0100

    new commit

--- mvp-frontend/src/main.jsx
+++ mvp-frontend/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 // import EnterMessage from './pages/entermessage/EnterMessage'
-// import EMRedirect from './pages/emredirect/EMRedirect'
-// import Register from './pages/register/Register'
+import EMRedirect from './pages/emredirect/EMRedirect'
+import Register from './pages/register/Register'
 // import Home from './pages/home/home'
-// import Login from './pages/login/Login'
-import Messages from './pages/messages/Messages'
+import Login from './pages/login/Login'
+// import Messages from './pages/messages/Messages'
 // import './index.css'
 // import App from './App.jsx'
 
@@ -16,6 +17,23 @@ createRoot(document.getElementById('root')).render(
     {/* <Register /> */}
     {/* <Home /> */}
     {/* <Login /> */}
-    <Messages />
+    {/* <Messages /> */}
+
+
+
+     <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/register" element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/emredirect' element={<EMRedirect />} />
+        {/* <Route path='/reset_password' element={<ResetPassword />} /> */}
+        {/* <Route path='/home' element={<ProtectedRoute><Dash /></ProtectedRoute>} /> */}
+        
+      </Routes>   
+    </BrowserRouter>
+
+
+
   </StrictMode>,
 )
--- mvp-frontend/src/pages/emredirect/EMRedirectCom1.jsx
+++ mvp-frontend/src/pages/emredirect/EMRedirectCom1.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const EMRedirectCom1 = () => {
 
     return(
         <>
         <div className="emredirect_contain">
             <div className="emrcon2">
-                <p>Create an account. <a href="">register</a></p>
-                <p>Already have an account? <a href="">login</a></p>
+                <p>Create an account. <Link to="/register">register</Link></p>
+                <p>Already have an account? <Link to="/login">login</Link></p>
             </div>
         </div>
         </>
